efcba84346f7866cc025c3dcffcc4bb4ec34a969
unknown <a-f-r@hotmail.es>
2021-07-21 01:09:23 +0200

    Add dynamic routes and avoid multiple pages simultaneously using Switch component

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Products() {
   return (
     <div>
       <h1>The Products page</h1>
+      <ul>
+        <li>
+          <Link to='/products/p1'>Book</Link>
+        </li>
+        <li>
+          <Link to='/products/p2'>Carpet</Link>
+        </li>
+        <li>
+          <Link to='/products/p3'>Online Course</Link>
+        </li>
+      </ul>
     </div>
   );
 }
